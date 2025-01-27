099dbebb840518f7931eba40235d206960825063
altynbek07 <altynbek.kazezov.97@gmail.com>
2025-01-27 18:57:07 +0500

    refactor: restructure main function to return a ServeMux instance

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,21 @@ import (
 	"net/http"
 )
 
-func main() {
+func App() *http.ServeMux {
 	router := http.NewServeMux()
 
 	// Handler
 	webhook.NewWebhookHandler(router)
 
+	return router
+}
+
+func main() {
+	app := App()
+
 	server := http.Server{
 		Addr:    ":8081",
-		Handler: router,
+		Handler: app,
 	}
 	fmt.Println("Server is listening on port 8081")
 	server.ListenAndServe()
